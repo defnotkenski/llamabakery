4f11169df1d56f87d6739275568413c444d4c218
Kenski <98671760+defnotkenski@users.noreply.github.com>
2025-08-30 15:44:58 -0700

    improvements & bug fixes

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from ollama import chat
 import argparse
+from mcp_tools import weather_penis
 
 
-def main(msg: str | None) -> None:
-    if msg is None:
-        msg = "Why is the sky blue?"
-
+def main(msg: str) -> None:
     messages = [
         {
             "role": "user",
@@ -13,20 +11,29 @@ def main(msg: str | None) -> None:
         }
     ]
 
-    stream_response = chat(model="dolphin3:8b-llama3.1-fp16", messages=messages, stream=True)
+    stream_response = chat(
+        model="dolphin3:8b-llama3.1-fp16",
+        messages=messages,
+        tools=[weather_penis],
+        stream=True,
+    )
 
-    # print(response.message.content)
     for chunk in stream_response:
+        # Print content.
         print(chunk.message.content, end="", flush=True)
 
-    print("")
+        # Print tool calls.
+        if chunk.message.tool_calls:
+            print(chunk.message.tool_calls)
+
+    print()
     return
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description="LlamaBakery entry script.")
 
-    p.add_argument("-m", "--message", required=False, help="Message to send to the LLM.")
+    p.add_argument("-m", "--message", required=True, help="Message to send to the LLM.")
     args = p.parse_args()
 
     main(msg=args.message)
---
+++ mcp_tools.py
@@ -0,0 +1,2 @@
+def weather_penis(loc: str) -> str:
+    return f"{loc}: 68_Penis"

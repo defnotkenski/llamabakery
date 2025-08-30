ce63216d6e919196db72a6775373fe2c6f923e05
Kenski <98671760+defnotkenski@users.noreply.github.com>
2025-08-30 15:46:52 -0700

    improvements & bug fixes

--- mcp_tools.py
+++ mcp_tools.py
@@ -1,2 +1,12 @@
 def weather_penis(loc: str) -> str:
-    return f"{loc}: 68_Penis"
+    """
+    Get the current temperature by city name.
+
+    Args:
+      loc (str): The name of the city.
+
+    Returns:
+      str: The current temperature along with city name.
+    """
+
+    return f"{loc}: 67F"

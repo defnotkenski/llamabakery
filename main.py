0877953f7d5f125bc2d9ce271086f0f8364fedc5
Kenski <98671760+defnotkenski@users.noreply.github.com>
2025-09-01 19:40:04 -0700

    improvements & bug fixes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from textwrap import dedent
+from typing import Sequence
+
 from ollama import chat, Message
 import argparse
 from mcp_tools import get_weather, remember_event
@@ -37,6 +39,7 @@ def main(msg: str) -> None:
     user_msg = Message(role="user", content=msg)
 
     messages: list[Message] = [default_system_msg, user_msg]
+    toolcalls: Sequence[Message.ToolCall] | None = None
 
     while True:
         assistant_parts: list[str] = []
@@ -54,44 +57,52 @@ def main(msg: str) -> None:
                 print(chunk.message.content, end="", flush=True)
                 assistant_parts.append(chunk.message.content)
 
+            if chunk.message.tool_calls:
+                toolcalls = chunk.message.tool_calls
+
         print()  # Single newline after the final chunk for readability.
 
         # === Post streaming. ===
         assistant_txt = "".join(assistant_parts).strip()
 
         # === Always append assistant turn (content + tool calls) as a typed Message. ===
-        assistant_msg = Message(role="assistant", content=assistant_txt)
+        assistant_msg = Message(role="assistant", content=assistant_txt, tool_calls=toolcalls)
         messages.append(assistant_msg)
 
+        if toolcalls is None:
+            break
+
+        print(toolcalls)
+
         # === Try to parse a tool-call json. ===
-        tool_call = None
-        try:
-            obj = json.loads(assistant_txt)
-            if isinstance(obj, dict):
-                tool_call = obj
-
-        except JSONDecodeError:
-            pass
-
-        if not tool_call:
-            break  # no tools requested => done.
-
-        # === Execute tool and append result. ===
-        tool_name = tool_call.get("tool")
-        tool_params = tool_call.get("args")
-
-        tool_func = TOOL_REGISTRY.get(tool_name)
-        assert tool_func, f"Unknown tool: {tool_name}"
-
-        try:
-            tool_result = tool_func(**tool_params)
-        except TypeError:
-            # Fallback for positional args.
-            tool_result = tool_func(tool_params.get("loc", ""))
-        except Exception as e:
-            tool_result = f"ERROR: tool {tool_name} failed: {e}"
-
-        messages.append(Message(role="tool", tool_name=tool_name, content=str(tool_result)))
+        # tool_call = None
+        # try:
+        #     obj = json.loads(assistant_txt)
+        #     if isinstance(obj, dict):
+        #         tool_call = obj
+        #
+        # except JSONDecodeError:
+        #     pass
+        #
+        # if not tool_call:
+        #     break  # no tools requested => done.
+        #
+        # # === Execute tool and append result. ===
+        # tool_name = tool_call.get("tool")
+        # tool_params = tool_call.get("args")
+        #
+        # tool_func = TOOL_REGISTRY.get(tool_name)
+        # assert tool_func, f"Unknown tool: {tool_name}"
+        #
+        # try:
+        #     tool_result = tool_func(**tool_params)
+        # except TypeError:
+        #     # Fallback for positional args.
+        #     tool_result = tool_func(tool_params.get("loc", ""))
+        # except Exception as e:
+        #     tool_result = f"ERROR: tool {tool_name} failed: {e}"
+        #
+        # messages.append(Message(role="tool", tool_name=tool_name, content=str(tool_result)))
 
     print()
     return
